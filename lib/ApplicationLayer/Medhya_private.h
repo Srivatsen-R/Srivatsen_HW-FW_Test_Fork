04e71479f44dfc4478899353a2a458a4223bc3fe
Srivatsen <srivatsen.r@eulermotors.com>
2025-02-17 13:30:17 +0530

    new file:   lib/ApplicationLayer/Medhya.c 	new file:   lib/ApplicationLayer/Medhya.h 	new file:   lib/ApplicationLayer/Medhya_data.c 	new file:   lib/ApplicationLayer/Medhya_private.h 	new file:   lib/ApplicationLayer/Medhya_types.h

---
+++ lib/ApplicationLayer/Medhya_private.h
@@ -0,0 +1,32 @@
+/*
+ * Medhya_private.h
+ *
+ * Code generation for model "Medhya".
+ *
+ * Model version              : 18.83
+ * Simulink Coder version : 24.2 (R2024b) 21-Jun-2024
+ * C source code generated on : Thu Feb 13 12:24:30 2025
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: STMicroelectronics->ST10/Super10
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef Medhya_private_h_
+#define Medhya_private_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+#include "Medhya_types.h"
+#include "Medhya.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+
+extern real_T rt_modd_snf(real_T u0, real_T u1);
+extern real_T look2_binlcpw(real_T u0, real_T u1, const real_T bp0[], const
+  real_T bp1[], const real_T table[], const uint32_T maxIndex[], uint32_T stride);
+extern real_T look2_binlcapw(real_T u0, real_T u1, const real_T bp0[], const
+  real_T bp1[], const real_T table[], const uint32_T maxIndex[], uint32_T stride);
+
+#endif                                 /* Medhya_private_h_ */
---
+++ lib/ApplicationLayer/Medhya_types.h
@@ -0,0 +1,26 @@
+/*
+ * Medhya_types.h
+ *
+ * Code generation for model "Medhya".
+ *
+ * Model version              : 18.83
+ * Simulink Coder version : 24.2 (R2024b) 21-Jun-2024
+ * C source code generated on : Thu Feb 13 12:24:30 2025
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: STMicroelectronics->ST10/Super10
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef Medhya_types_h_
+#define Medhya_types_h_
+
+/* Parameters (default storage) */
+typedef struct P_Medhya_T_ P_Medhya_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_Medhya_T RT_MODEL_Medhya_T;
+
+#endif                                 /* Medhya_types_h_ */
